424195a3f7fd7ec9ee5929c5d6f34cd3a73dbe0d
almanzarangel <56279276+almanzarangel@users.noreply.github.com>
2019-11-07 22:49:08 -0400

    Update FirstSeleniumTest.java

--- FirstSeleniumTest.java
+++ FirstSeleniumTest.java
@@ -8,14 +8,14 @@ public class FirstSeleniumTest {
 	public static void main(String[] args) {
 		
 		
-		String projectLocation = System.getProperty("user.dir");
+		String projectLocation = System.getProperty("user.dir"); //this method will help getting the project location
 			
 		System.setProperty("webdriver.chrome.driver", projectLocation+ "\\Lib\\chromedriver\\chromedriver.exe");
 	
 		
 		WebDriver driver=new ChromeDriver();
 		
-		driver.get("http://seleniumhq.org");
+		driver.get("http://facebook.com");
 		driver.manage().window().maximize();
 		driver.quit();
 	}
